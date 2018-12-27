68991c8169a1ce89862569dcdfea5ec4fa7b4003
evereve@tuta.io <evereve@tuta.io>
2018-12-27 10:17:58 +0300

    fff

--- GLApp/main.cpp
+++ GLApp/main.cpp
@@ -18,6 +18,36 @@ int main() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); 
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); // OpenGL 3.3
 
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // new way, without glBegin()
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // for MacOS not to crash
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE); //non-resizable window
+
+	// Create a GLFWwindow object that we can use for GLFW's functions 
+	GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr);
+
+	int screenWidth, screenHeight;
+
+	glfwGetFramebufferSize(window, &screenWidth, &screenHeight);
+
+	if (nullptr == window) {
+
+		std::cout << "Failed to create GLFW window" << std::endl;
+
+		glfwTerminate();
+		return EXIT_FAILURE;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	// Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions 
+	glewExperimental = GL_TRUE;
+
+	// Initialize GLEW to setup the OpenGL Function pointers 
+	if (GLEW_OK != glewInit())
+	{
+		std::cout << "Failed to initialize GLEW" << std::endl;
+		return EXIT_FAILURE;
+	}
+
 
 }
